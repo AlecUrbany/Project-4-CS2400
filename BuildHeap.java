ef5574cef1398082996aaae0a387783acf5ede06
AlecUrbany <alecurbany@gmail.com>
2022-04-20 23:14:54 -0700

    Co-authored-by: NasserA <NasserA12433@users.noreply.github.com>

--- BuildHeap.java
+++ BuildHeap.java
@@ -2,17 +2,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.IOException;
-import java.sql.Array;
-import java.io.FileWriter;
+import java.io.PrintStream;
 public class BuildHeap 
 {
 	private static int size;
 	private int[] arr;
 	private static int arraycopy;
 
-	// To heapify a subtree rooted with node i which is
-	// an index in arr[].Nn is size of heap
-	
+
  static int swapCount=0;
 	static void heapify(int arr[], int n, int i)
 	{
@@ -40,9 +37,26 @@ public class BuildHeap
 			heapify(arr, n, largest);
 			swapCount++;
 		}
-		
+
 	}
-	
+	static int removeHeap(int arr[], int n)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			int lastItem = arr[n-1];
+
+			arr[0] = lastItem;
+			n = n - 1;
+			heapify(arr, n, 0);
+		}
+		System.out.print("Heap after 10 removals: ");
+		
+		for (int i = 0; i < 10; ++i)
+			System.out.print(arr[i] + " ");
+
+		System.out.println();
+		return n;
+	}
 	// Function to build a Max-Heap from the Array
 	static void buildHeap(int arr[], int n)
 	{
@@ -59,9 +73,9 @@ public class BuildHeap
 	}
 		public static void buildheap2(int arr[], int n)
 		{
-			size= arr.length;
+			size = arr.length;
 			
-			arr= new int[arraycopy];
+			arr = new int[arraycopy];
 			
 			for(int i=0;i<arr.length;i++)
 			{
@@ -72,21 +86,11 @@ public class BuildHeap
         }
 		}
 
-	public int removeHeap()
-	{
-		if (this.size<1)
-			return -1;
-		int temp = this.arr[0];
-		this.arr[0] = this.arr[size-1];
-		this.arr[size-1] = temp;
-		size--;
-		return this.arr[size];
-	}
 	// A utility function to print the array
 	// representation of Heap
 	static void printHeap(int arr[], int n)
 	{
-		System.out.println("Array representation of Heap is:");
+		System.out.print("Heap built using optimal method ");
 		
 		for (int i = 0; i < 10; ++i)
 			System.out.print(arr[i] + " ");
@@ -98,12 +102,26 @@ public class BuildHeap
 	// Driver Code
 	public static void main(String args[]) throws FileNotFoundException
 	{
+		//File Printer
+		File output = null;
+		try 
+		{
+			output = new File("heapOutput.txt");
+			PrintStream stream = new PrintStream(output);
+			System.setOut(stream);
+		} 
+		catch (IOException error) 
+		{
+		  System.out.println("Oopsies, there's a mistake");
+		  error.printStackTrace();
+		}
 		//File parser
 		File unsortedData = new File("data_random.txt");
         int i = 0; 
-        int ii = 0;
         int array[];
+		
         array = new int[100];
+		
 	    try (Scanner scanFile = new Scanner(unsortedData))
         {
             array[i] = scanFile.nextInt();
@@ -114,34 +132,19 @@ public class BuildHeap
             }
 
 		int n = array.length;
-
+		System.out.println("=====================================================================");
 		buildHeap(array, n);
 		printHeap(array, n);
-		System.out.println("THIS IS BUILD HEAP 2 BELOW");
-		
+		System.out.println("Number of swaps in the heap creation: "+swapCount);
+		swapCount=0;
+		removeHeap(array, n);
+		System.out.println("Number of swaps in the heap removal code "+swapCount);
+		System.out.println("\nTHIS IS BUILD HEAP 2 BELOW:");
 		buildheap2(array, n);
-
 		printHeap(array, n);
+		System.out.println("=====================================================================");
 		System.out.println("The total swap count is "+swapCount);	
-		//File maker
-		// FileWriter output = null;
-		// try 
-		// {
-		//   output = new FileWriter("heapOutput.txt");
-		// 	for (String line : lines)
-		// 	{
-		// 		output.write(line + "\n");
-		// 	}
-		// } 
-		// catch (IOException error) 
-		// {
-		//   System.out.println("Oopsies, there's a mistake");
-		//   error.printStackTrace();
-		// }
 		}
 	}
-
-	
-	
 }
 
